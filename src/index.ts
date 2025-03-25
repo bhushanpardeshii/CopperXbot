6497e05a72043aeeac254d97744bc62a69e50e8c
Bhushan Pardeshi <bhushanpardeshi549@gmail.com>
2025-03-25 16:51:29 +0530

    get handler

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req: any, res: any) => {
+    res.send("bot is running");
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 dotenv.config();
